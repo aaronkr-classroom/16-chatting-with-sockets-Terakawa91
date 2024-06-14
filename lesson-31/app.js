0eb7f935cdd3816e4b50053363cddae3fe27ac88
Terakawa91 <68772480+Terakawa91@users.noreply.github.com>
2024-06-14 11:58:06 +0000

    lesson_30_fin

--- lesson-31/app.js
+++ lesson-31/app.js
@@ -95,19 +95,14 @@ app.use((req, res, next) => {
  * =====================================================================
  */
 
-// 애플리케이션에 Mongoose 설정
-const mongoose = require("mongoose"), // mongoose를 요청
-  dbName = "aaronkr";
-
+const mongoose = require("mongoose"); // mongoose를 요청
 // 데이터베이스 연결 설정
-mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
-  useNewUrlParser: true,
-});
-
-// 연결되면 메시지를 보냄
-const db = mongoose.connection;
+mongoose.connect(
+  "mongodb+srv://krissvector58:LdOReXBPN87fODa6@ut-node.0qoahfe.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node/ut-node"
+);
+const db = mongoose.connection; 
 db.once("open", () => {
-  console.log(`Connected to ${dbName} MongoDB using Mongoose!`);
+  console.log("Connected to MONGODB!!");
 });
 
 /**
